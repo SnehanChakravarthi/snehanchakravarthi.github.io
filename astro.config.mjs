b9dfc6b973d01389ff4d19f9f820944ad8bced1d
Snehan Chakravarthi <snehanchakravarthi@gmail.com>
2024-02-24 14:47:36 +0100

    Final Fix 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	}),
 	prefetch: true,
 	integrations: [
 		react(),
